08c9f9dc3acb918a7cf7791dca21f483e2b4e2d3
Jose-ant-1 <jgonsol1510@g.educaand.es>
2025-10-06 14:29:49 +0200

    modificacion del proyecto

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 console.log('Happy developing ✨')
+
+let texto;
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("pagina cargada");
+    const mostrar = document.getElementById('mostrar');
+
+    fetch("email-password-recovery-code.csv")
+        .then(response => response.text())  // obtenemos el texto del CSV
+        .then(data => {
+            texto = data;  // aquí tienes todo el contenido del CSV en data
+            window.texto = data;
+            console.log(texto)
+            // si quieres, guárdalo en una variable global
+            window.data = texto;
+            mostrar.innerHTML = texto;
+        });
+
+});
+
+
+
+
+
+
